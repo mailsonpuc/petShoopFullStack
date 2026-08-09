0bfbe88adc930489b2720232adb972f64ec60a2d
mailsonpuc <mailson.costa@proton.me>
2026-08-09 02:10:54 -0300

    feat: dotnet format

--- src/backend/PetShoop.Infrastructure/Identity/Models/RegisterModel.cs
+++ src/backend/PetShoop.Infrastructure/Identity/Models/RegisterModel.cs
@@ -10,7 +10,7 @@ public class RegisterModel
 
     [EmailAddress(ErrorMessage = "Email is required")]
     public string? Email { get; set; }
-    
+
 
     [Required(ErrorMessage = "Password is required")]
     public string? Password { get; set; }
