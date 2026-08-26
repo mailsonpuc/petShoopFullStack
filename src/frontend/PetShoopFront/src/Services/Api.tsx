08ce32b2f8e87ea673e6b77ff1a4f40902ffd4cd
mailsonpuc <mailson.costa@proton.me>
2026-08-26 09:20:27 -0300

    url backend azure

--- src/frontend/PetShoopFront/src/Services/Api.tsx
+++ src/frontend/PetShoopFront/src/Services/Api.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     
-    //https://apipetshoop-f8b7cpataca3fhd8.canadacentral-01.azurewebsites.net/api
-    baseURL: "http://localhost:5100/api",
+    //http://localhost:5100/api 
+    baseURL: "https://apipetshoop-f8b7cpataca3fhd8.canadacentral-01.azurewebsites.net/api",
     
 });
 
